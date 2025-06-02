853b31e2b80c6cf88907c833385c30f0ba76d404
xMiguelac <mikemiguelangel@hotmail.com>
2025-06-01 20:38:01 -0400

    Descrypt function added

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ const inputNumber = document.getElementById("inputNumber");
 const textBox1 = document.getElementById("textBox1");
 const textBox2 = document.getElementById("textBox2");
 const infoError = document.getElementById("info-error");
+const title1 = document.querySelector(".Title-box1");
+const title2 = document.querySelector(".Title-box2");
 
-// Limita el input del número entre 1 y 27
-inputNumber.setAttribute("min", 1);
+// Limita el input del número entre 0 y 27
+inputNumber.setAttribute("min", 0);
 inputNumber.setAttribute("max", 26);
 
 // Validación del input numérico
 inputNumber.addEventListener("input", () => {
   const value = parseInt(inputNumber.value, 10);
-  if (isNaN(value) || value < 1 || value > 26) {
+  if (isNaN(value) || value < 0 || value > 26) {
     inputNumber.value = ""; // Limpiar si no es válido
     textBox1.value = ""; // Limpiar el texto cifrado
     textBox2.value = ""; // Limpiar el texto cifrado
@@ -58,6 +60,38 @@ function encrypt(text, shift) {
 
   return result;
 }
+
+// Función de desencriptado
+function decryptText(){
+  const alphabet = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"; 
+  inputNumber.value = 0;
+  textBox1.disabled = false; // Habilitar el campo de texto
+  textBox2.disabled = false; // Habilitar el campo de texto cifrado
+
+  const encryptedText = textBox2.value;
+  const [shiftStr, text] = encryptedText.split(":");
+  let result = "";
+
+  const shift = parseInt(shiftStr, 10);
+
+  for (let char of text.trim()) {
+    const index = alphabet.indexOf(char);
+    if (index === -1) {
+      result += char;
+    } else {
+      // Ajustar índice para evitar negativos
+      let newIndex = (index - shift) % 27;
+      if (newIndex < 0) newIndex += 27;
+      result += alphabet[newIndex];
+    }
+  }
+
+  textBox1.value = result; // Mostrar el texto desencriptado en el primer cuadro de texto
+  return result;
+
+}
+
+
 
 // Escuchar cambios en el texto
 textBox1.addEventListener("input", () => {
